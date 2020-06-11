ebf5cd796f211e27d8916c03ae5acffc478f4a2e
JaydenZhou <zhoujun2000@163.com>
2020-06-11 16:52:13 +0800

    实体校验处理

--- src/main/java/com/markerhub/controller/UserController.java
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -2,13 +2,12 @@ package com.markerhub.controller;
 
 
 import com.markerhub.common.lang.Result;
+import com.markerhub.entity.User;
 import com.markerhub.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -23,10 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserService userService;
+
+    @RequiresAuthentication
     @GetMapping("/{id}")
     public Object test(@PathVariable("id") Long id) {
 //        return userService.getById(id);
-        return Result.succ(userService.getById(id));
+        User user = userService.getById(id);
+        return Result.succ(user);
+    }
+
+    // 测试实体校验
+    @PostMapping("/save")
+    public Object testUser(@Validated @RequestBody User user) {
+        return Result.succ(user);
     }
 
 }
